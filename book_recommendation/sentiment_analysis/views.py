7988243e9714107da06d61f94f883afbce8879df
s1f102201672 <107388803+s1f102201672@users.noreply.github.com>
2024-10-22 22:21:17 +0900

    Form作成（分析途中）
    ニュートラルのままになる

--- book_recommendation/sentiment_analysis/views.py
+++ book_recommendation/sentiment_analysis/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ReviewForm  # ここでReviewFormをインポート
+from textblob import TextBlob  # TextBlobを使用
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Sentiment Analysis is working!")
+    result = ''
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():  # is_valid()メソッドを使用
+            review_text = form.cleaned_data['review']
+            # 感情分析を行う
+            analysis = TextBlob(review_text)
+            if analysis.sentiment.polarity > 0:
+                result = 'Positive'
+            elif analysis.sentiment.polarity < 0:
+                result = 'Negative'
+            else:
+                result = 'Neutral'
+    else:
+        form = ReviewForm()
+    
+    return render(request, 'sentiment_analysis/index.html', {'form': form, 'result': result})
